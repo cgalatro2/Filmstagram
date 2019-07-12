794fa881a10dd4cb6f89f6eacfda18232d1ccd7b
Chase Galatro <chasegalatro@gmail.com>
2019-07-12 11:11:20 -0400

    changed port for deploy

--- app.js
+++ app.js
@@ -64,7 +64,7 @@ app.use("/", indexRoutes);
 app.use("/movies", movieRoutes);
 app.use("/movies/:id/comments", commentRoutes);
 
-app.listen(3000, () => {
-	console.log('listening on port 3000');
-});
-// app.listen(process.env.PORT, process.env.IP);
+// app.listen(3000, () => {
+// 	console.log('listening on port 3000');
+// });
+app.listen(process.env.PORT, process.env.IP);
